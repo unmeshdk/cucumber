759e97f6a461aee8a41c16ef0a8ff0cd6f1308b3
Kulkarni <unmesh.a.kulkarni@capgemini.com>
2018-05-06 13:37:09 +0530

    success

--- seleniumdemo2/src/test/java/com/uk/gitregi/GitRegistrationTest.java
+++ seleniumdemo2/src/test/java/com/uk/gitregi/GitRegistrationTest.java
@@ -45,15 +45,15 @@ public class GitRegistrationTest {
 	
 	@Test
 	public void testForRegistrationSuccess() {
-		registrationPage.setUsername("mytest123456udk");
+		registrationPage.setUsername("mytest123456udk1234");
 		registrationPage.setPassword("mytest123456");
-		registrationPage.setEmail("mytest123456@gmail.com");
+		registrationPage.setEmail("mytest123456udk@gmail.com");
 		registrationPage.clickSubmitButton();
 		//String message1 = 
 		//String message2 = driver.findElement(By.xpath("//p[@class='lead']")).getText();
-		String actualErrorMessage = "@" + driver.findElement(By.xpath("//p[@class='lead']//strong")).getText(); 
-		System.out.println("success Message :- " + actualErrorMessage);
-		String expectedErrorMessage = "@mytest123456";
-		Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+		String actualSuccessMessage =  driver.findElement(By.xpath("//p[@class='lead']//strong")).getText(); 
+		System.out.println("success Message :- " + actualSuccessMessage);
+		String expectedsuccessMessage = "@mytest123456udk1234";
+		Assert.assertEquals(expectedsuccessMessage, actualSuccessMessage);
 	}
 }
